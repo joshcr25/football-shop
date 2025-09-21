05b60d3d0235dc63078769ab365f71fda28ffed6
joshcr25 <Josh Christmas Rommlynn>
2025-09-21 23:44:21 +0700

    Menyelesaikan tugas 4

--- main/models.py
+++ main/models.py
@@ -1,12 +1,13 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
         ('jersey', 'Jersey (Baju seragam)'),
         ('bola', 'Bola'),
 ]
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) # tambahkan ini
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length = 255)
     price = models.IntegerField(default = 200000)
